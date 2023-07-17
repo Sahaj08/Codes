006cf82c1edbc46b210e31e6f58fdf3daefaa3a7
Sahaj08 <123300697+Sahaj08@users.noreply.github.com>
2023-07-17 21:40:42 +0530

    Add files via upload

---
+++ arrayy.java
@@ -0,0 +1,26 @@
+
+package practice;
+
+
+//to find where the element is present inside array or not
+
+
+public class arrayy {
+ public static void main(String[]args) {
+	 int[] number= {10,12,34,54,12};
+	 int element=19;
+	 for(int i=0; i<5; i++) {
+		 if(element==number[i]) {
+			 System.out.println(" the element is present in the array");
+			 
+		
+		 }
+		 else { System.out.println("not present");
+		 break;}
+		
+	 }
+	     
+	 }
+}
+
+
