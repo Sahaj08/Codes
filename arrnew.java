735b56d623abe6423dcc5dd55538f1dca24fdf41
Sahaj08 <123300697+Sahaj08@users.noreply.github.com>
2023-07-19 21:26:58 +0530

    Add files via upload

---
+++ arrnew.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class arrnew {
+	public static void main(String args[]) {
+int arr[]= {12,13,14};
+for(int i=0; i<3;i++) {
+	System.out.println(arr[i]);
+}
+System.out.println();
+
+for(int value:arr) {
+	System.out.println(value);
+}
+}
+}
